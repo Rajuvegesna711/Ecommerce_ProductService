e3780c841dac5d07dc38acaa42323a556149b89f
Rajuvegesna711 <rajuvegesna711@gmail.com>
2024-06-23 03:13:56 +0530

    no message

--- src/main/java/com/vegesna/scalerproject/ProductServiceApplication.java
+++ src/main/java/com/vegesna/scalerproject/ProductServiceApplication.java
@@ -4,16 +4,11 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-
 @SpringBootApplication
-public class ScalerProjectApplication {
+public class ProductServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ScalerProjectApplication.class, args);
+        SpringApplication.run(ProductServiceApplication.class, args);
 
 //        List<Integer> z = new ArrayList<>(Arrays.asList(1,2,3,4,5));
 //        Iterator<?> x = z.iterator();
